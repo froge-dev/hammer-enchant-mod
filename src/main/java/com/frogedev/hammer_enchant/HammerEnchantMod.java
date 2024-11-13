a448a5fe68ffe4938a099ac3e3f13239aadfa0b9
Radu Schirliu <radu.schirliu@gmail.com>
2024-11-13 09:23:50 -0800

    chore: remove unused code; format

--- src/main/java/com/frogedev/hammer_enchant/HammerEnchantMod.java
+++ src/main/java/com/frogedev/hammer_enchant/HammerEnchantMod.java
@@ -2,17 +2,11 @@ package com.frogedev.hammer_enchant;
 
 import com.frogedev.hammer_enchant.datagen.Generators;
 import com.mojang.logging.LogUtils;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.NonNullLazy;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 @Mod(HammerEnchantMod.MOD_ID)
@@ -26,21 +20,11 @@ public class HammerEnchantMod {
         // Prevent JVM from optimizing away these classes...
         new Generators();
 
-        modEventBus.addListener(this::onModCommonSetup);
         ModEnchantments.register(modEventBus);
 
-        MinecraftForge.EVENT_BUS.addListener(this::onServerStarting);
         MinecraftForge.EVENT_BUS.register(Generators.class);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.COMMON, ModConfig.SPEC);
-    }
-
-    private void onModCommonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
-    }
-
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
     }
 }
--- src/main/java/com/frogedev/hammer_enchant/util/HammerHelper.java
+++ src/main/java/com/frogedev/hammer_enchant/util/HammerHelper.java
@@ -2,9 +2,7 @@ package com.frogedev.hammer_enchant.util;
 
 import com.frogedev.hammer_enchant.ModEnchantments;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Vec3i;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
@@ -15,7 +13,6 @@ import java.util.List;
 import java.util.UUID;
 
 import static com.frogedev.hammer_enchant.util.HammerShapeHelper.getCandidateBlockPositions;
-import static com.frogedev.hammer_enchant.util.HammerShapeHelper.getMiningSize;
 
 public class HammerHelper {
     public static boolean hasHammerModifiers(ItemStack tool) {
@@ -42,7 +39,7 @@ public class HammerHelper {
             return false;
         }
 
-        if(!handler.isToolCorrectType(tool)){
+        if (!handler.isToolCorrectType(tool)) {
             return false;
         }
 
