853b9b920fd31d3c19bcc9ae55acbd87d99b1ed9
Tallon Hodge <tallonkh@gmail.com>
2025-03-03 19:52:19 -0800

    update version & fix crash

--- src/main/java/com/frogedev/hammer_enchant/event/client/ToolRenderEvents.java
+++ src/main/java/com/frogedev/hammer_enchant/event/client/ToolRenderEvents.java
@@ -35,28 +35,11 @@ public class ToolRenderEvents {
     /**
      * Maximum number of blocks from the iterator to render
      */
-    private static final int MAX_BLOCKS = 60;
+    private static final int MAX_BLOCKS = 75;
 
     public record FloatColor(float r, float g, float b){};
 
-    private static Field field_LevelRenderer_DestroyingBlocks;
-
     private static final BaseHammerHandler[] HANDLERS = {ToolUseHandler.INSTANCE, MiningHandler.INSTANCE};
-
-    static {
-        field_LevelRenderer_DestroyingBlocks = ObfuscationReflectionHelper.findField(LevelRenderer.class, "destroyingBlocks");
-        field_LevelRenderer_DestroyingBlocks.setAccessible(true);
-    }
-
-    private static Int2ObjectMap<BlockDestructionProgress> getBlockDestructionProgress(LevelRenderer levelRenderer) {
-        try {
-            return (Int2ObjectMap<BlockDestructionProgress>) field_LevelRenderer_DestroyingBlocks.get(levelRenderer);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
 
     /**
      * Renders the outline on the extra blocks
@@ -130,99 +113,4 @@ public class ToolRenderEvents {
 
         LevelRenderer.renderVoxelShape(poseStack, vertexBuilder, shape, -camPos.x, -camPos.y, -camPos.z, pRed, pGreen, pBlue, 1.0F, false);
     }
-
-//    /**
-//     * Renders the block damage process on the extra blocks
-//     */
-//    @SubscribeEvent
-//    public static void onRenderLevelStage(RenderLevelStageEvent event) {
-//        if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_BLOCK_ENTITIES) {
-//            return;
-//        }
-//
-//        Player player = Minecraft.getInstance().player;
-//        Level level = player.level();
-//
-//        ItemStack tool = player.getMainHandItem();
-//        if (!HammerHelper.hasHammerModifiers(tool)) {
-//            return;
-//        }
-//
-//        if (!(Minecraft.getInstance().hitResult instanceof BlockHitResult blockTrace)) {
-//            return;
-//        }
-//
-//        BlockPos origin = blockTrace.getBlockPos();
-//        ToolMode activeMode = ToolMode.None;
-//
-//        // Find the active tool mode.
-//        for (ToolMode candidateMode : MODE_ATTEMPT_ORDER) {
-//            if (candidateMode.handler.isToolCorrectType(tool) && candidateMode.handler.testOrigin(level, player, tool, origin)) {
-//                activeMode = candidateMode;
-//                break;
-//            }
-//        }
-//
-//        // If no tool mode qualifies, do nothing.
-//        if (activeMode == ToolMode.None) {
-//            return;
-//        }
-//
-//        Iterator<BlockPos> breakableBlocks = HammerShapeHelper.getCandidateBlockPositions(
-//                player,
-//                tool,
-//                Minecraft.getInstance().hitResult,
-//                origin,
-//                activeMode.handler
-//        );
-//
-//        LevelRenderer levelRenderer = Minecraft.getInstance().levelRenderer;
-//        Int2ObjectMap<BlockDestructionProgress> destroyingBlocks = getBlockDestructionProgress(levelRenderer);
-//
-//        if (destroyingBlocks == null) {
-//            return;
-//        }
-//
-//        BlockDestructionProgress destroyProgress = null;
-//        for (Int2ObjectMap.Entry<BlockDestructionProgress> entry : destroyingBlocks.int2ObjectEntrySet()) {
-//            if (entry.getValue().getPos().equals(origin)) {
-//                destroyProgress = entry.getValue();
-//                break;
-//            }
-//        }
-//        if (destroyProgress == null) {
-//            return;
-//        }
-//
-//        BlockRenderDispatcher blockRenderer = Minecraft.getInstance().getBlockRenderer();
-//        PoseStack matrices = event.getPoseStack();
-//        PoseStack poseStack = event.getPoseStack();
-//        RenderBuffers renderBuffers = Minecraft.getInstance().renderBuffers();
-//        MultiBufferSource.BufferSource breakBufferSource = renderBuffers.crumblingBufferSource();
-//        RenderType destroyRenderType = ModelBakery.DESTROY_TYPES.get(destroyProgress.getProgress());
-//
-//        // Translate back to origin
-//        Camera camera = event.getCamera();
-//        double x = camera.getPosition().x;
-//        double y = camera.getPosition().y;
-//        double z = camera.getPosition().z;
-//        poseStack.pushPose();
-//        poseStack.translate(-x, -y, -z);
-//
-//        while (breakableBlocks.hasNext()) {
-//            BlockPos blockPos = breakableBlocks.next();
-//            BlockState blockState = level.getBlockState(blockPos);
-//
-//            poseStack.pushPose();
-//            poseStack.translate(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-//            PoseStack.Pose lastPose = poseStack.last();
-//
-//            VertexConsumer vertexConsumer = new SheetedDecalTextureGenerator(breakBufferSource.getBuffer(destroyRenderType), lastPose.pose(), lastPose.normal(), 1.0F);
-//            blockRenderer.renderBreakingTexture(blockState, blockPos, level, poseStack, vertexConsumer, ModelData.EMPTY);
-//
-//            poseStack.popPose();
-//        }
-//
-//        poseStack.popPose();
-//    }
 }
